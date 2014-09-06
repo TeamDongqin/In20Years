4010a2a9bb6e6bae5a4b6377753660a6237ecfaa
dongqin03 <lingshan.zheng.i@icloud.com>
2014-09-06 11:00:35 +0800

    Add project prototype
    Add

---
+++ In20Years/In20Years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  In20Years
+//
+//  Created by zheng lingshan on 14-9-6.
+//  Copyright (c) 2014年 zheng lingshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ In20Years/In20YearsTests/In20YearsTests.swift
@@ -0,0 +1,36 @@
+//
+//  In20YearsTests.swift
+//  In20YearsTests
+//
+//  Created by zheng lingshan on 14-9-6.
+//  Copyright (c) 2014年 zheng lingshan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class In20YearsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
